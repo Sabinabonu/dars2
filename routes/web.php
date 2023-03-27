b66bda73db195b46e6c0fe14bdb61215413bcb3a
*SpRiTe* <mirzamahmudovasabinabonu@gmail.com>
2023-03-27 08:29:05 +0500

    integer misollari

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,7 @@ Route::get('/begin5/{a}', function($a){
     $v=pow($a,3);
     return "Hajmi:".$v."<br> To'la sirti:".$l;
 });
+
 Route::get('/begin6/{a}/{b}/{c}', function($a,$b,$c){
    $s=2*($a*$b+$b*$c+$a*$c);
     return 'To`la sirt:'.$s."<br> Hajmi:".$v;
@@ -76,3 +77,66 @@ Route::get('/begin10/{a}/{b}', function($a,$b){
 
 });
 
+
+Route::get('/int1/{l}', function($l){
+    $n=floor($l/100);
+    return $n;
+});
+
+Route::get('/int2/{m}', function($m){
+    $n=floor($m/1000);
+    return $n;
+});
+
+Route::get('/int3/{x}', function($x){
+    $n=floor($x/1024);
+    return $n;
+});
+
+Route::get('/int4/{a}/{b}', function($a, $b){
+    $n=floor($a/$b);
+    return $n;
+});
+
+Route::get('/int5/{a}/{b}', function($a, $b){
+    $n=floor($a/$b);
+    $x=$a-$n*$b;
+    return $n."<br>".$x;
+});
+
+Route::get('/int6/{n}', function($n){
+    $a=floor($n/10);
+    $b=$n%10;
+    return "O'nlar xonasidagi son = ".$a."<br> Birlar Xonasidagi son=".$b;
+});
+
+
+Route::get('/int7/{n}', function($n){
+    $a=floor($n/10);
+    $b=$n%10;
+    $x=$a+$b;
+    return "Raqamlar yihindisi = ".$x;
+});
+
+Route::get('/int8/{n}', function($n){
+    $a=floor($n/10);
+    $b=$n%10;
+    
+    return $b.$a;
+});
+
+Route::get('/int9/{n}', function($n){
+    $a=floor($n/100);
+    
+    
+    return "Yuzlar Homasidagi son=". $a;
+});
+
+Route::get('/int10/{n}', function($n){
+    $a=floor($n/10)%10;
+    $b=$n%100;
+    
+    return "O'nliklar Homasidagi son=". $a."<br>Birliklar honasidagi son=".$b;
+});
+
+?>
